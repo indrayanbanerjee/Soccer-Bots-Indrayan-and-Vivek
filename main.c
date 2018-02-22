44337f7694567e633e28215856604d4e1b144fe5
Indrayan Banerjee <36719332+indrayanbanerjee@users.noreply.github.com>
2018-02-22 14:06:38 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+#pragma config(Sensor, in1,    lineT,          sensorLineFollower)
+#pragma config(Sensor, dgtl1,  button,         sensorTouch)
+#pragma config(Sensor, dgtl10, green,          sensorLEDtoVCC)
+#pragma config(Sensor, dgtl11, red,            sensorLEDtoVCC)
+#pragma config(Sensor, dgtl12, yellow,         sensorLEDtoVCC)
+#pragma config(Motor,  port1,           trap,          tmotorVex393_HBridge, openLoop)
+#pragma config(Motor,  port10,          move,          tmotorVex393_HBridge, openLoop)
 task main()
 {
   turnLEDOn(green);
